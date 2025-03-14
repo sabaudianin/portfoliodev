df22a3343b1ae6ae10dad440824781dade85c05b
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 <sabaudianin@gmail.com>
2025-03-14 21:27:17 +0100

    theme tests updated

--- tests/ThemeToggle.spec.tsx
+++ tests/ThemeToggle.spec.tsx
@@ -12,4 +12,29 @@ describe("ThemeToggle component Tests", () => {
     render(<ThemeToggle />);
     expect(screen.getByRole("button")).toBeInTheDocument();
   });
+  it("render theme icon", () => {
+    (useTheme as jest.Mock).mockReturnValue({
+      theme: "light",
+      mounted: true,
+      toggleTheme: vi.fn(),
+    });
+
+    render(<ThemeToggle />);
+    expect(screen.getByRole("button")).toHaveClass("bg-gray-200");
+  });
+
+  it("Toggle theme between dark/light", () => {
+    const toggleThemeMock = vi.fn();
+    (useTheme as jest.Mock).mockReturnValue({
+      theme: "light",
+      mounted: true,
+      toggleTheme: toggleThemeMock,
+    });
+
+    render(<ThemeToggle />);
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+
+    expect(toggleThemeMock).toHaveBeenCalledTimes(1);
+  });
 });
