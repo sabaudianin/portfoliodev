7a036a5529f0544bb729639e4fc05fc88b65660f
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 <sabaudianin@gmail.com>
2025-05-11 14:45:34 +0200

    added footer tests

---
+++ src/layouts/Footer/Footer.spec.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { Footer } from "./Footer";
+
+describe("Footer Tests", () => {
+  beforeEach(() => {
+    render(<Footer />);
+  });
+  it("Render component properly", () => {
+    expect(screen.getByText("rafbobbob@gmail.com")).toBeInTheDocument();
+    expect(screen.getByText("+48 793386445")).toBeInTheDocument();
+  });
+  it("Render icons", () => {
+    const icons = screen.getAllByRole("img", { hidden: true });
+    expect(icons.length).toBeGreaterThanOrEqual(2);
+  });
+});
--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { FaPhone, FaEnvelope } from "react-icons/fa";
 
 export const Footer = () => {
@@ -5,10 +6,19 @@ export const Footer = () => {
     <section className="w-full text-center p-4 bg-gradient-to-l from-blue-400 to-cyan-400 shadow-lg text-black text-xs">
       <div className="">
         <div className="flex items-center justify-center gap-2">
-          <FaPhone className="text-bold " /> +48 793386445
+          <FaPhone
+            role="img"
+            aria-label="phone"
+            className="text-bold "
+          />{" "}
+          +48 793386445
         </div>
         <div className="flex items-center justify-center gap-2">
-          <FaEnvelope className=" " />
+          <FaEnvelope
+            role="img"
+            aria-label="envelope"
+            className="text-bold "
+          />
           rafbobbob@gmail.com
         </div>
       </div>
