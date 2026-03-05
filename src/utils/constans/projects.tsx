cebd4b39a48e787bdb275d6eb679c7afe168a243
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 <sabaudianin@gmail.com>
2026-03-05 20:21:55 +0100

    update projects, left 8

--- src/utils/constans/projects.tsx
+++ src/utils/constans/projects.tsx
@@ -47,50 +47,17 @@ export const projects = [
     repo: "https://github.com/sabaudianin/ai-image",
     link: "https://micro-frontend-ai-neon.vercel.app/",
   },
-  {
-    title: "My-Elements",
-    image: "/projectsView/myelements.avif",
-    description: "Private UI elements library build in CSS or Tailwind",
-    repo: "https://github.com/sabaudianin/my-elements",
-    link: "https://github.com/sabaudianin/my-elements",
-  },
+
   {
     title: "React Fun",
     image: "/projectsView/api.avif",
     description:
-      "Bundle with three Apps: Array Methods App, Pesel Check App, Api Integrations App.",
+      "Bundle of three Apps: Array Methods App, Pesel Check App, Api Integrations App.",
     repo: "https://github.com/sabaudianin/Tablica-Validacja-Api",
     link: "https://peselchecker.netlify.app/",
   },
 
-  {
-    title: "St-System",
-    image: "/projectsView/st.avif",
-    description: "Landing Page",
-    repo: "https://github.com/sabaudianin/st-system",
-    link: "https://st-system.vercel.app/",
-  },
-  {
-    title: "Coding Bros",
-    image: "/projectsView/CBsmall.png",
-    description: "Simple Page build with Webpack",
-    repo: "https://github.com/sabaudianin/NicePage",
-    link: "https://nicepage-ba5fe.web.app/",
-  },
-  {
-    title: "PostComments",
-    image: "/projectsView/PC.png",
-    description: "App with CRUD operations",
-    repo: "https://github.com/sabaudianin/PostComments",
-    link: "https://fir-server-bf9dc.web.app/",
-  },
-  {
-    title: "TimePlaner",
-    image: "/projectsView/TP.png",
-    description: "Task Tracker App for kids",
-    repo: "https://github.com/sabaudianin/TimePlaner",
-    link: "https://timeplaner.netlify.app/",
-  },
+
   {
     title: "CV",
     image: "/projectsView/cv.avif",
