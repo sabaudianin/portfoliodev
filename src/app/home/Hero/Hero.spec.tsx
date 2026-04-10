ddff11a95447eb74ab09d23e3af02ce7a9f8254c
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 <sabaudianin@gmail.com>
2026-04-10 11:03:02 +0200

    add vitest ui interface, fix test Hero

--- src/app/home/Hero/Hero.spec.tsx
+++ src/app/home/Hero/Hero.spec.tsx
@@ -1,40 +1,46 @@
 import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
+import type { ComponentProps, HTMLAttributes } from "react";
+import Image from "next/image";
 import Hero from "./Hero";
-import { useTheme } from "@/hooks/useTheme/useTheme";
 
-// Mock useTheme
-vi.mock("@/hooks/useTheme");
+
+// mock next/image
+type ImageProps = ComponentProps<typeof Image>;
+vi.mock("next/image", () => ({
+  /* eslint-disable @next/next/no-img-element */
+  default: (props: ImageProps) => <img {...props} />,
+}));
+
+// mock framer-motion
+
+type DivProps = HTMLAttributes<HTMLDivElement>;
+type PProps = HTMLAttributes<HTMLParagraphElement>;
+type H2Props = HTMLAttributes<HTMLHeadingElement>;
+vi.mock("framer-motion", () => ({
+  motion: {
+    div: (props: DivProps) => <div {...props} />,
+    p: (props: PProps) => <p {...props} />,
+    h2: (props: H2Props) => <h2 {...props} />,
+  },
+}));
 
 describe("Hero Component", () => {
-  it("should not render if not mounted", () => {
-    vi.mocked(useTheme).mockReturnValue({
-      theme: "light",
-      mounted: false,
-      toggleTheme: vi.fn(),
-    });
-
+  it("should render hero content", () => {
     render(<Hero />);
-    expect(screen.queryByText("Hi there, I am")).not.toBeInTheDocument();
-  });
 
-  it("should render hero content when mounted", () => {
-    vi.mocked(useTheme).mockReturnValue({
-      theme: "dark",
-      mounted: true,
-      toggleTheme: vi.fn(),
-    });
+    expect(screen.getByText(/Hi there, I'm/i)).toBeInTheDocument();
 
-    render(<Hero />);
-    expect(screen.getByText("Hi there, I am")).toBeInTheDocument();
     expect(
       screen.getByRole("img", { name: "Rafal Bobko" })
     ).toBeInTheDocument();
+
     expect(
-      screen.getByRole("button", { name: /hire me/i })
+      screen.getByRole("link", { name: /see my projects/i })
     ).toBeInTheDocument();
+
     expect(
-      screen.getByRole("button", { name: /about me/i })
+      screen.getByRole("link", { name: /download cv/i })
     ).toBeInTheDocument();
   });
 });
